ce6b19c38a568f7f41327df943aa534c4e888758
Jonatan Padilla <jonatan.padilla@soytul.com>
2023-06-16 23:01:02 -0500

    Initial Commit

---
+++ firebaseLogin/firebaseLoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebaseLoginApp.swift
+//  firebaseLogin
+//
+//  Created by Jonatan Padilla on 16/06/23.
+//
+
+import SwiftUI
+
+@main
+struct firebaseLoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
